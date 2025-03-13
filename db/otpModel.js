d252aa056ed284772437975f27a46e0825d17460
faizan <sfaizu9820@gmail.com>
2025-03-13 06:14:34 +0530

    ADDED ttl index

--- db/otpModel.js
+++ db/otpModel.js
@@ -1,29 +1,15 @@
 const mongoose = require("mongoose");
 
 
-
-//pehchanotp schema
-
 const OtpSchema = new mongoose.Schema(
-
- {
-  mobile: { type: String ,index: true },
-
-  otpValue: { type: String },
-
-  otpCount: {type : Number},
-
-  createdAt:{type:Date,default:Date.now,expires:86400}
-
-  //createdAt:{type:Date,default:Date.now,expires:60}
-
- },
-
- {
-
-  timestamps: true,
-
- }
+    {
+        mobile: { type: String, index: true },
+        otpValue: { type: String },
+        otpCount: { type: Number }
+    },
+    {
+        timestamps: true,
+    }
 
 );
 
